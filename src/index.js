ffa2d586f90cf8c051129114f170a978285f0d45
codeandcodeandcode <codepkcode49@gmail.com>
2022-06-27 09:40:03 +0300

    commit tree 9af20db4fb5a5efffe93e48d6afe87ff9040b175

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 console.log('%c HI', 'color: firebrick')
 
+console.log('%c HI', 'color: firebrick')
+
 document.addEventListener("DOMContentLoaded", ()=>{
     let lis = [];
     //showed al images from fetch
@@ -14,26 +16,27 @@ document.addEventListener("DOMContentLoaded", ()=>{
         });
     })
 
-        //Fetched all dog breeds
+    //fetched al dog breeds
     fetch(breedUrl).then(a=>a.json()).then(b=>{
         let all = b.message;
         for (const k in all) {
             all[k].forEach(i => {
                 let li = document.createElement("li");
-                    li.innerHTML = i;
-                    lis.push(li)
+                li.innerHTML = i;
+                lis.push(li)
     document.querySelector("#dog-breeds").append(li);
     li.onclick = () => li.style.color = "green"
-                });
-            }
-        })
-        //On select change, filtered throught the lists array and displayed them
-        document.querySelector("#breed-dropdown").addEventListener("change", f=>{
-            let ss = f.target.value;
-            let c = lis.filter(b=>b.textContent.charAt(0)==ss);
-            document.querySelector("#dog-breeds").innerHTML = ""
-            c.forEach(s=>{
-                document.querySelector("#dog-breeds").append(s);
-            })
+            });
+        }
+    })
+
+    //on select change, filtered throught the lists array and displayed them
+    document.querySelector("#breed-dropdown").addEventListener("change", f=>{
+        let ss = f.target.value;
+        let c = lis.filter(b=>b.textContent.charAt(0)==ss);
+        document.querySelector("#dog-breeds").innerHTML = ""
+        c.forEach(s=>{
+            document.querySelector("#dog-breeds").append(s);
         })
     })
+})
